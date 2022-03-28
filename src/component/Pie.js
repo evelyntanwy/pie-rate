18e479b5119e6c995acaec71bb13fa9366843f7e
Evelyn Tan <evetan.wy@gmail.com>
2022-03-28 21:14:09 +1300

    Add number keyboard to Pie component page

---
+++ src/component/Pie.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Pie() {
+  return (
+    <div>
+      <h3>Pie</h3>
+      <h4>Keyboard</h4>
+      <button value="1">1</button>
+      <button value="2">2</button>
+      <button value="3">3</button>
+      <button value="4">4</button>
+      <button value="5">5</button>
+      <button value="6">6</button>
+      <button value="7">7</button>
+      <button value="8">8</button>
+      <button value="9">9</button>
+      <button value="0">0</button>
+      <button value=".">.</button>
+    </div>
+  );
+}
+
+export default Pie;
