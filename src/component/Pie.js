f760598c374a1bf9f8e3e629284bc33dcd34328e
Evelyn Tan <evetan.wy@gmail.com>
2022-04-01 15:48:42 +1300

    Add input for number onClick

--- src/component/Pie.js
+++ src/component/Pie.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Pie() {
+  const [number, setNumber] = useState([]);
+
+  const handleChange = (evt) => {
+    evt.preventDefault();
+    console.log(number);
+    setNumber(evt.target.value, ...number);
+  };
+
   return (
     <div>
       <h3>Pie</h3>
       <h4>Keyboard</h4>
-      <button value="1">1</button>
-      <button value="2">2</button>
-      <button value="3">3</button>
-      <button value="4">4</button>
-      <button value="5">5</button>
-      <button value="6">6</button>
-      <button value="7">7</button>
-      <button value="8">8</button>
-      <button value="9">9</button>
-      <button value="0">0</button>
-      <button value=".">.</button>
+      {number}
+      <div>
+        <button onClick={handleChange} value="1">
+          1
+        </button>
+        <button onClick={handleChange} value="3">
+          3
+        </button>
+        <button onClick={handleChange} value="2">
+          2
+        </button>
+        <button onClick={handleChange} value="4">
+          4
+        </button>
+        <button onClick={handleChange} value="5">
+          5
+        </button>
+        <button onClick={handleChange} value="6">
+          6
+        </button>
+        <button onClick={handleChange} value="7">
+          7
+        </button>
+        <button onClick={handleChange} value="8">
+          8
+        </button>
+        <button onClick={handleChange} value="9">
+          9
+        </button>
+        <button onClick={handleChange} value="0">
+          0
+        </button>
+        <button onClick={handleChange} value=".">
+          .
+        </button>
+      </div>
     </div>
   );
 }
